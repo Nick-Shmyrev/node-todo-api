10387579fe80d51b384998ef7892dae69e153ba6
Nick-Shmyrev <nr.shmyrev@gmail.com>
2017-11-13 23:21:29 -0500

    Add test for GET /users/me & POST /users

---
+++ server/tests/seed/seed.js
@@ -0,0 +1,60 @@
+var {ObjectID} = require('mongodb');
+var jwt = require('jsonwebtoken');
+const {Todo} = require('./../../models/todo.js');
+const {User} = require('./../../models/user.js');
+
+const user1Id = new ObjectID();
+const user2Id = new ObjectID();
+
+const users = [
+  {
+    _id: user1Id,
+    email: 'something@somewhere.ca',
+    password: 'user1psw',
+    tokens: [
+      {
+        access: 'auth',
+        token:  jwt.sign({_id: user1Id, access: 'auth'}, 'abc123').toString()
+      }
+    ]
+  },
+  {
+    _id: user2Id,
+    email: 'something@something.else',
+    password: 'user2psw',
+  }
+]
+
+const todos = [
+  {
+    _id: new ObjectID(),
+    text: 'First test to do'
+  },
+  {
+    _id: new ObjectID(),
+    text: 'Second test to do'
+  },
+  {
+    _id: new ObjectID(),
+    text: 'Third test',
+    completed: true,
+    completedAt: 333
+  }
+];
+
+const populateUsers = (done) => {
+  User.remove({}).then(() => {
+    var userOne = new User(users[0]).save();
+    var userTwo = new User(users[1]).save();
+
+    return Promise.all([userOne, userTwo]);
+  }).then(() => done());
+};
+
+const populateTodos = (done) => {
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
+};
+
+module.exports = {todos, populateTodos, users, populateUsers};
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -2,31 +2,14 @@ const expect = require('expect');
 const request = require('supertest');
 const {ObjectID} = require('mongodb');
 
-const {app} = require('./../server');
-const {Todo} = require('./../models/todo');
+const {app} = require('./../server.js');
+const {Todo} = require('./../models/todo.js');
+const {User} = require('./../models/user.js');
+const {todos, populateTodos, users, populateUsers} = require('./seed/seed.js');
 
-const todos = [
-  {
-    _id: new ObjectID(),
-    text: 'First test to do'
-  },
-  {
-    _id: new ObjectID(),
-    text: 'Second test to do'
-  },
-  {
-    _id: new ObjectID(),
-    text: 'Third test',
-    completed: true,
-    completedAt: 333
-  }
-];
 
-beforeEach((done) => {
-  Todo.remove({}).then(() => {
-    return Todo.insertMany(todos);
-  }).then(() => done());
-});
+beforeEach(populateUsers);
+beforeEach(populateTodos);
 
 describe('POST /todos', () => {
   it('should create a new todo', (done) => {
@@ -202,6 +185,76 @@ describe('PATCH /todos/:id', () => {
     request(app)
       .delete(`/todos/${id}`)
       .expect(404)
+      .end(done);
+  });
+});
+
+
+describe('GET /users/me', () => {
+  it('should return user if authenticated', (done) => {
+    request(app)
+      .get('/users/me')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body._id).toBe(users[0]._id.toHexString());
+        expect(res.body.email).toBe(users[0].email);
+      })
+      .end(done);
+  });
+
+  it('should return 401 if not authenticated', (done) => {
+    request(app)
+      .get('/users/me')
+      .expect(401)
+      .expect((res) => {
+        expect(res.body).toEqual({});
+      })
+      .end(done);
+  });
+});
+
+describe('POST /users', () => {
+  it('should create a user', (done) => {
+    var email = 'example@example.com';
+    var password = '123qwerty';
+
+    request(app)
+      .post('/users')
+      .send({email, password})
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toExist();
+        expect(res.body._id).toExist();
+        expect(res.body.email).toBe(email);
+      })
+      .end((err) => {
+        if (err) {
+          return done(err)
+        }
+
+        User.findOne({email}).then((user) => {
+          expect(user).toExist();
+          expect(user.password).toNotBe(password);
+          done();
+        })
+      });
+
+  });
+
+  it('should return validation errors if request is invalid', (done) => {
+    request(app)
+      .post('/users')
+      .send({email: 'Not a real email', password: '2shrt'})
+      .expect(400)
+      .end(done);
+  });
+
+  it('should not create user if email is not unique', (done) => {
+    request(app)
+      .post('/users')
+      .send({email: users[0].email, password: "No1Cares"})
+      .expect(400)
       .end(done);
   });
 });
